a71f80f378c097efeae2ff1cb316af8b89425cd8
salehchy46 <salehchyctg@gmail.com>
2025-08-23 00:37:06 +0600

    reviews collection data is fetched form the backend

--- index.js
+++ index.js
@@ -27,12 +27,17 @@ async function run() {
     await client.connect();
 
     const menuCollection = client.db('bistroDb').collection('menu');
+    const reviewsCollection = client.db('bistroDb').collection('reviews');
 
     app.get('/menu', async(req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
     })
-    
+    app.get('/reviews', async(req, res) => {
+      const result = await reviewsCollection.find().toArray();
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
