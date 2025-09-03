557e628e16bbf0b6dddbeb9d566726eb1c74617c
salehchy46 <salehchyctg@gmail.com>
2025-09-03 21:27:37 +0600

    delete and update opertion is added

--- index.js
+++ index.js
@@ -50,6 +50,25 @@ async function run() {
       res.send(result);
     })
 
+    app.patch('/users/admin/:id', async(req, res) => {
+      const id = req.params.id;
+      const filter = {_id : new ObjectId(id)};
+      const updateDoc ={
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })
+
+    app.delete('/user/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)};
+      const result = await userCollection.delete(query);
+      res.send(result);
+    })
+
     //menu related API
     app.get('/menu', async (req, res) => {
       const result = await menuCollection.find().toArray();
