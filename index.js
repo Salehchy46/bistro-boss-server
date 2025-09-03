13d994f46c9853cd1b5a96151bd508c48296c385
salehchy46 <salehchyctg@gmail.com>
2025-09-03 10:56:47 +0600

    user from google login

--- index.js
+++ index.js
@@ -34,6 +34,13 @@ async function run() {
     //user related API
     app.post('/users', async(req, res) => {
       const user = req.body;
+      //insert email if user doesn't exist:
+      //You can do this many ways (1. email unique, 2. upsert, 3. simple checking)
+      const query = {email : user.email}
+      const existingUser = await userCollection.findOne(query);
+      if(existingUser){
+        return res.send({message: 'user already exists', insertedId: null})
+      }
       const result = await userCollection.insertOne(user);
       res.send(result);
     })
